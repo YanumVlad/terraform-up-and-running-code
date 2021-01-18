5cacc041327fc0d906762cecba01de8659c03d8d
Vlad Yanum <v.yanum@akveo.com>
2021-01-18 23:17:27 +0300

    terraform var renaming

--- code/terraform/03-terraform-state/file-layout-example/modules/services/webserver-cluster/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/modules/services/webserver-cluster/main.tf
@@ -13,7 +13,7 @@ locals {
 resource "aws_launch_configuration" "example" {
   image_id        = var.ami
   instance_type   = var.instance_type
-  security_groups = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.cluster_instance.id]
 
   user_data = data.template_file.user_data.rendered
 
@@ -76,13 +76,13 @@ resource "aws_autoscaling_group" "example" {
     }
   }
 }
-resource "aws_security_group" "instance" {
+resource "aws_security_group" "cluster_instance" {
   name = "${var.cluster_name}-instance"
 }
 
 resource "aws_security_group_rule" "allow_http_inbound_inst" {
   type              = "ingress"
-  security_group_id = aws_security_group.instance.id
+  security_group_id = aws_security_group.cluster_instance.id
 
   from_port   = local.http_port
   to_port     = var.server_port
--- code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/main.tf
@@ -87,3 +87,9 @@ EOF
 # output "if_else_directive" {
 #   value = "Hello, %{if var.name != ""}${var.name}%{else}(unnamed)%{endif}"
 # }
+
+resource "aws_iam_user" "existing_user" {
+  # You should change this to the username of an IAM user that already
+  # exists so you can practice using the terraform import command
+  name = "developer1"
+}
