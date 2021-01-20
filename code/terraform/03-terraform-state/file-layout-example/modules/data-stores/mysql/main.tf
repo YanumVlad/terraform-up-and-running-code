e20a95e8f1f509e8f70dfadfad15308d41ba2f23
Vlad Yanum <v.yanum@akveo.com>
2021-01-20 19:21:33 +0300

    Terragrunt update

--- code/terraform/03-terraform-state/file-layout-example/modules/data-stores/mysql/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/modules/data-stores/mysql/main.tf
@@ -1,6 +1,15 @@
 terraform {
-
+  # Require any 0.12.x version of Terraform
   required_version = ">= 0.12, <= 0.13"
+
+  # Partial configuration. The rest will be filled in by Terragrunt.
+  backend "s3" {}
+}
+provider "aws" {
+  region = "us-east-2"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
 }
 
 resource "aws_db_instance" "example" {
