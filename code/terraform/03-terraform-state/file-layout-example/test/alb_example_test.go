4da2d704875cdf4c54d096abf62fddaa388d48d7
Vlad Yanum <v.yanum@akveo.com>
2021-01-22 14:11:00 +0300

    ALB test was added

---
+++ code/terraform/03-terraform-state/file-layout-example/test/alb_example_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"testing"
+	"fmt"
+	"time"
+)
+
+func TestAlbExample(t *testing.T)  {
+	opts := &terraform.Options{
+		// You should update this relative path to point at your alb
+		// example directory!
+		TerraformDir: "../examples/alb",
+	}
+
+	// Clean up everything at the end of the test
+	defer terraform.Destroy(t, opts)
+
+	// Deploy the example
+	terraform.InitAndApply(t, opts)
+
+	// Get the URL of the ALB
+	albDnsName := terraform.OutputRequired(t, opts, "alb_dns_name")
+	url := fmt.Sprintf("http://%s", albDnsName)
+
+	expectedStatus := 404
+	expectedBody := "404: page not found"	
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+
+	http_helper.HttpGetWithRetry(
+		t,
+		url,
+		nil,
+		expectedStatus,
+		expectedBody,
+		maxRetries,
+		timeBetweenRetries,
+	)
+
+}
--- code/terraform/03-terraform-state/file-layout-example/test/go.mod
+++ code/terraform/03-terraform-state/file-layout-example/test/go.mod
@@ -1,3 +1,5 @@
 module example.com
 
 go 1.13
+
+require github.com/gruntwork-io/terratest v0.31.5
